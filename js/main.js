f31554be23b311feb0b8e62ee80e6d14619273b0
brodsa <brodinova@gmail.com>
2022-07-01 12:31:49 +0200

    copying to git

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function showAbstractVT() {
+    var x = document.getElementById("abstractVT");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function showAbstractAA() {
+    var x = document.getElementById("abstractAA");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function showAbstractAD() {
+    var x = document.getElementById("abstractAD");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
